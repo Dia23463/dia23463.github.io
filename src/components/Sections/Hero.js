d87b11b69ac5f3ddfc73b9aa4b74ce07c38f071d
Dia Sutaria <154221931+Dia23463@users.noreply.github.com>
2025-08-28 17:44:39 -0400

    Update Hero.js

--- src/components/Sections/Hero.js
+++ src/components/Sections/Hero.js
@@ -129,7 +129,7 @@ const Hero = () => {
                 <img
                   className="relative w-5 h-5"
                   alt=""
-                  src="/email.svg"
+                  src=Image={`${process.env.PUBLIC_URL}/email.svg`}
                 />
               </button>
               <button
@@ -139,7 +139,7 @@ const Hero = () => {
                 <img
                   className="relative w-5 h-5"
                   alt=""
-                  src="/Linkedin.svg"
+                  src=Image={`${process.env.PUBLIC_URL}/Linkedin.svg`}
                 />
               </button>
               <button
@@ -149,7 +149,7 @@ const Hero = () => {
                 <img
                   className="relative w-5 h-5"
                   alt=""
-                  src="/Github.svg"
+                  src=Image={`${process.env.PUBLIC_URL}/Github.svg`}
                 />
               </button>
               <button
@@ -159,7 +159,8 @@ const Hero = () => {
                 <img
                   className="relative w-5 h-5"
                   alt=""
-                  src="/Medium.svg"
+                  src={`${process.env.PUBLIC_URL}/Medium.svg`}
+
                 />
               </button>
             </div>
@@ -167,5 +168,6 @@ const Hero = () => {
         </div>
       </section>
     )};
+
 
 export default Hero;
